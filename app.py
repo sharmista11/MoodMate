0090750d24d29c7087ccd2ff10b75a402ac2aa7c
sharmista11 <sharmistamobile@gmail.com>
2025-02-17 21:49:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 app = Flask(__name__)
 
 # Download NLTK resources
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
